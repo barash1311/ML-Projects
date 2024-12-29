1a9a45448fafd1ab1c1d78a056f0eb3ad539ac8b
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-12-30 00:34:05 +0530

    update

---
+++ Day-19-Streamlit/app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+#title of the application
+st.title("hello streamlit")
+
+##display a  simple text
+st.write("this is a simple text")
+##create a data frame
+df=pd.DataFrame({
+    "first column":[1,2,3,4,5],
+    "second column":[10,20,30,40,50]
+})
+##display the data frame
+st.write(df)
+
+##create line chart
+char_Data=pd.DataFrame(
+    np.random.randn(20,4),columns=["a","b","c","d"]
+)
+st.line_chart(char_Data)
